f6eba1be94d19677f6793ec4fdc499ec2aecd5e7
peter <fr33wayz@gmail.com>
2017-09-14 12:48:51 +0100

    added logic to register the event mechanism to be bounded to it self

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window._mu = (function () {
     function validateFiles(uploadedFiles) {
         for (var x = 0; x < uploadedFiles.length; x++) {
-            console.log(uploadedFiles[x])
+            console.log(uploadedFiles[x]);
         }
     }
 
@@ -25,6 +25,25 @@ window._mu = (function () {
         imageContainer.setAttribute('id', 'imgPreview')
         return imageContainer
     }
+    function sendUpload (url, jsonData) {
+        return new Promise(function(success, fail) {
+            var req  = new XMLHttpRequest();
+            req.open('POST', url, true );
+            req.addEventListener('load', function(){
+                if (req.status < 400) {
+                    success(req)
+                } else {
+                    fail(new Error('Network Error ....'))
+                }
+            });
+
+            req.addEventListener('error', function() {
+                fail(req);
+            });
+
+            req.send(jsonData)
+        });
+    }
 
     function FSMediaUploader(els) {
         this.els = els;
@@ -40,28 +59,54 @@ window._mu = (function () {
         show: function (uploadUrl) {
             // create our uploader widget
             this.uploadUrl = uploadUrl;
-            this.els.innerHTML = this.html()
-            this.els.querySelector('#uploadButton').addEventListener('click', this.processUpload)
-            var uploaderInput = this.els.querySelector('#fileUpload')
-            uploaderInput.addEventListener('change', this.renderFilesInfo)
-            uploaderInput.addEventListener('dragover', this.dropOver);
-            uploaderInput.addEventListener('drop', this.renderFilesInfo)
+            this.els.innerHTML = this.html();
+        },
 
+
+        /**
+         * Hack to allow our application 
+         * to have reference to it self
+         * Since event listener this are bounded
+         * to the event trigger
+         */
+        registerEvents: function() {
+            var self = this;
+            this.els.querySelector('#uploadButton').addEventListener('click', function(e){
+                return self.processUpload(e)
+            })
+            var uploaderInput = this.els.querySelector('#fileUpload')
+            uploaderInput.addEventListener('change', function(e){
+                return self.renderFilesInfo(e);
+            })
+            uploaderInput.addEventListener('dragover', function(e){
+                return self.dropOver(e);
+            });
+            uploaderInput.addEventListener('drop', function(e){
+                return this.renderFilesInfo(e)
+            });
         },
 
         processUpload: function (event) {
             event.preventDefault();
-            const uploadedFiles = document.getElementById('fileUpload');
-            console.log('Files ', uploadedFiles.files)
-            validateFiles(uploadedFiles.files);
-            // const formData = new FormData(uploadedFiles);
-            // console.log("Uploading Form Input is ", formData);
+            console.log('Self ', this.querySelector('#fileUpload'))
+            const uploadedInputFiles = document.getElementById('fileUpload');
+            const files = uploadedInputFiles.files;
+            var formData = new FormData();            
+            for (var f = 0; f < files.length; f++) {
+                formData.append(`photo_${f}`, files[f]);
+            }
+            // validateFiles(uploadedFiles.files);
+            sendUpload(this.uploadUrl, formData).then(function(response) {
+                console.log('Response from server ', JSON.stringify(response))
+            }).catch(function(error) {
+                console.log('Error loading images')
+            })
+
         },
 
         renderFilesInfo: function (event) {
             // console.log('This is processing', this.)
             var uploadedInputFiles = document.getElementById('fileUpload');
-            console.log('Uploaded files ', uploadedInputFiles)
             var files = uploadedInputFiles.files;
             var infoElm = document.getElementById('fileInfos');
             var imageContainer = document.getElementById('imagesContainer');
@@ -74,13 +119,6 @@ window._mu = (function () {
                 imageContainer.appendChild(imageToShow)
                 convertImageStream(imageToShow, files[i])
             }
-            // console.
-            // show  the preview images 
-            // var loadedImages = document.querySelectorAll('#imgPreview');
-            // for (var li = 0; li < loadedImages.length; li++) {
-            //     ;
-            // }
-
         },
 
 
@@ -124,7 +162,9 @@ window._mu = (function () {
                 results.push(callback.call(this, this[i], i));
             }
             return results;
-        }
+        },
+
+        
     }
 
     var mu = {
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 var mediaUploader = _mu.init('modal');
 var uploadUrl = 'http://demo.safsms.cloud'
 mediaUploader.show(uploadUrl);
+mediaUploader.registerEvents()
